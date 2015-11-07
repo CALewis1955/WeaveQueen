1baeeaf75847181727ac34c3c04aa3c575d11968
Christopher Lewis <clewis916@gmail.com>
2015-11-07 13:34:46 -0500

    Completed functionality for votes.html

--- app/scripts/controllers/battles.js
+++ app/scripts/controllers/battles.js
@@ -16,29 +16,35 @@ function battlesCtrl(DOMAIN, Weaves) {
   battle.weaves = Weaves.all();
   battle.contestant = Weaves.getRandom();
   battle.challenger = Weaves.getRandom();
+  while (battle.challenger === battle.contestant) {
+    battle.challenger = Weaves.getRandom();
+  };
   console.log(battle.contestant)
   console.log(battle.challenger);
   battle.vote = vote;
   battle.winner = Weaves.winner;
-
+  battle.loser = Weaves.loser;
+  if (battle.winner && battle.loser) {
+    (Weaves.loser.votes == 0) ? battle.winningPercent = 100 :
+      battle.winningPercent = (battle.winner.votes/(battle.loser.votes + battle.winner.votes)) * 100;
+  };
+  battle.losingPercent = 1 - battle.winningPercent;
+  console.log(battle.winningPercent);
 
   function vote() {
     var url = event.target.src;
     var name = '';
     var winner = url.slice(DOMAIN.length);
     battle.weaves.forEach(function(w){
-      console.log(w.source);
-      console.log(winner);
       if (w.source == winner) {
         w.votes += 1;
         battle.winner = w;
         Weaves.winner = w;
       }
-      console.log(battle.winner);
-
-   });
+      Weaves.loser = (battle.contestant !== battle.winner) ? battle.contestant : battle.challenger;
+    });
     console.log(battle.winner);
-   return Weaves.winner;
+   return {winner :Weaves.winner, loser: Weaves.loser};
   }
   console.log(Weaves.winner);
 }
